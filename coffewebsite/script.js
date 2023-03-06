63c5318471b621e641e846c5dd9a7b25c343fa1d
SamirShrestha332 <119655213+SamirShrestha332@users.noreply.github.com>
2023-03-06 17:10:53 +0545

    Add files via upload

---
+++ coffewebsite/script.js
@@ -0,0 +1,10 @@
+
+
+let change = document.getElementById('background1');
+var navigation = document.getElementById('header')
+
+
+function lightmode(){
+    change.classList.toggle('container_active');
+    navigation.classList.toggle('header-active');
+}
